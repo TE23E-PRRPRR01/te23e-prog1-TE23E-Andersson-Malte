9edcf44a0f63e8ec820c172b2ed9f8b18be8b760
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2024-08-26 14:31:43 +0200

    Tränat på cw, cr och mera

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Hej, världen!");
+Console.Write("Vad du heter?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej min käraste " + namn + "!");
+Console.WriteLine("Vad du gillar att göra på fritiden " + namn + "?");
+string fritiden = Console.ReadLine();
+Console.WriteLine("Jag gillar också att " + fritiden + " på fritiden!");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine("Du kollade på OS eller?");
+Console.WriteLine("Vilken sport du tycker var bäst?");
+
+
