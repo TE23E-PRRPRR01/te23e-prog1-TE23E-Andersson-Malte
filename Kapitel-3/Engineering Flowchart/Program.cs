731e3d1b3d611704ae773dc8c1577880c7543ad6
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2024-09-02 14:52:48 +0200

    tränat på if/else satser

---
+++ Kapitel-3/Engineering Flowchart/Program.cs
@@ -0,0 +1,61 @@
+﻿//Program som simulerar kända Engineering Flowchart
+using System.Reflection.Metadata;
+using Microsoft.VisualBasic;
+
+Console.Clear();
+
+
+//Visa en ASCII-art
+Console.WriteLine($"""
+        .--'''''''''--.
+     .'      .---.      '.
+    /    .-----------.    \
+   /        .-----.        \
+   |       .-.   .-.       |
+   |      /   \ /   \      |
+    \    | .-. | .-. |    /
+     '-._| | | | | | |_.-'
+         | '-' | '-' |
+          \___/ \___/
+       _.-'  /   \  `-._
+     .' _.--|     |--._ '.
+     ' _...-|     |-..._ '
+            |     |
+            '.___.'
+
+
+""");
+
+Console.Write("Does it move? (y/n) ");
+string answer = Console.ReadLine();
+
+//Kolla om svar är ja eller nej
+
+if (answer == "y")
+{
+    Console.Write("Shodit? (y/n) ");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("No problem");
+    }
+    else
+    {
+        Console.WriteLine("Use duck tape");
+    }
+}
+else
+{
+    Console.Write("Shodit? (y/n) ");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Use WD-40");
+    }
+    else
+    {
+        Console.WriteLine("No problem");
+
+    }
+
+}
---
+++ Kapitel-3/InteractiveStory/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Skriv i små bokstäver och klicka ENTER för att gå vidare.");
+Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Du vaknar upp i ett rum...");
+Console.ReadLine();
+Console.Write("Framför dig finns två dörrar, 1 och 2. Vilken väljer du? ");
+string svar = Console.ReadLine();
+Console.Clear();
+
+if (svar == "1")
+{
+    Console.WriteLine("Du passerar genom den första dörren...");
+    Console.ReadLine();
+      Console.WriteLine("och ner för en trappa...");
+    Console.ReadLine();
+    Console.Write("Du finner en halv brödbit på ett av trappstegen. Vill du äta den (j/n)? ");
+    svar = Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Du passerar genom den andra dörren...");
+}
+
