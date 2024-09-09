308de3ce4d56259300dd9360847c0c9bb776a753
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2024-09-09 14:50:26 +0200

    Tränat på heltal och konvertering med int.Parse

---
+++ Kapitel-3/AldersGranser/Program.cs
@@ -0,0 +1,78 @@
+﻿//Skapa ett program som skapar valfria åldersgränser
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("Ange ålder: ");
+string ålderText = Console.ReadLine();
+int ålder = int.Parse(ålderText);
+Console.WriteLine(" ");
+
+if (ålder > 17)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("DU FÅR SPELA:");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("COUNTER STRIKE");
+    Console.WriteLine("FORTNITE");
+    Console.WriteLine("MINECRAFT");
+    Console.WriteLine("ROBLOX");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("DU FÅR INTE SPELA:");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("COUNTER STRIKE");
+
+    if (ålder > 11)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(" ");
+        Console.WriteLine("DU FÅR SPELA:");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("FORTNITE");
+        Console.WriteLine("MINECRAFT");
+        Console.WriteLine("ROBLOX");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("FORTNITE");
+
+        if (ålder > 6)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ");
+              Console.WriteLine("DU FÅR SPELA:");
+              Console.ForegroundColor = ConsoleColor.White;
+              Console.WriteLine("MINECRAFT");
+              Console.WriteLine("ROBLOX");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("MINECRAFT");
+
+            if (ålder > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(" ");
+                Console.WriteLine("DU FÅR SPELA:");
+                Console.ForegroundColor = ConsoleColor.White;
+              Console.WriteLine("ROBLOX");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("ROBLOX");
+                Console.WriteLine(" ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("DU ÄR INTE ENS FÖDD");
+            }
+
+        }
+
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.White;
+System.Console.WriteLine(" ");
---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,46 @@
+﻿//Ett litet spel - gissa ett hemligt heltal
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(" ");
+Console.WriteLine("ETT LITET SPEL DÄR DU SKA GISSA ETT HEMLIGT HELTAL");
+Console.WriteLine(" ");
+
+//slumpa talet
+Console.ForegroundColor = ConsoleColor.White;
+int slumptalet = Random.Shared.Next(1, 101); //1-100
+Console.WriteLine($"Slumptalet är {slumptalet}");
+
+Console.Write("Gissa ett tal 1-100: ");
+//Läs in texten
+ string gissningText= Console.ReadLine();
+ //Konvertera texten till ett heltal
+int gissning = int.Parse(gissningText);
+
+if (gissning == slumptalet)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(" ");
+    Console.WriteLine("DU GISSADE RÄTT!");
+}
+else
+{
+   if (gissning < slumptalet)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+       Console.WriteLine(" ");
+    Console.WriteLine("DU GISSADE FÖR LÅGT!");
+}
+   else
+{
+   Console.ForegroundColor = ConsoleColor.Red;
+   Console.WriteLine(" ");
+   Console.WriteLine("DU GISSADE FÖR HÖGT!"); 
+}
+}
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(" ");
+
+
+
+
