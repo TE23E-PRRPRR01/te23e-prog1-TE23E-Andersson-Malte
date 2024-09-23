0d06892bd33f21aff825c20190b36e834d01ea46
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2024-09-23 14:49:06 +0200

    Vi har tränat på while-loopar and filhantering

---
+++ Kapitel-4/MenyProgram/Program.cs
@@ -0,0 +1,60 @@
+﻿//Litet program emd meny
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("MENYPROGRAM");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+//Variabel för valet
+string val = "";
+
+//Programloop som snurrar tills användaren säger avsluta
+while (true)
+{
+    //Visar en meny och läser in användarens val
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("VÄLJ ETT AN FÖLJANDE ALTERNATIV:");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("""
+    1. OMVANDLA EN TEXT TILL VERSALER
+    2. OMVANDLA EN TEXT TILL GEMENER
+    3. AVSLUTA
+    """);
+    Console.WriteLine(" ");
+    Console.Write("DITT SVAR: ");
+    val = Console.ReadLine();
+
+    //Hantera användarens val
+    if (val == "1")
+    {
+        Console.Clear();
+        Console.WriteLine("SKRIV IN EN TEXT: ");
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"TEXTEN I VERSALER BLIR: {texten}");
+    }
+    else
+    {
+        if (val == "2")
+        {
+        Console.Clear();
+        Console.WriteLine("SKRIV IN EN TEXT: ");
+        string texten = Console.ReadLine().ToLower();
+        Console.WriteLine($"TEXTEN I GEMENER BLIR: {texten}");
+        }
+        else
+        {
+            if (val == "3")
+            {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("AVSLUTAR...");
+            break;
+            }
+            else
+            {
+            Console.WriteLine("VÄNLIGEN VÄLJ ETT GILTIGT ALTERNATIV");
+            }
+        }
+    }
+}
---
+++ Kapitel-4/TextEditor/Program.cs
@@ -0,0 +1,71 @@
+﻿//Text editor
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+//Variabel för valet
+string val = "";
+
+//Programloop som snurrar tills användaren säger avsluta
+while (true)
+{
+    //Visar en meny och läser in användarens val
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("VÄLJ ETT AN FÖLJANDE ALTERNATIV:");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("""
+    1. SKRIV TILL FIL
+    2. LÄS FRÅN FIL
+    3. AVSLUTA
+    """);
+    Console.WriteLine(" ");
+    Console.Write("DITT SVAR: ");
+    val = Console.ReadLine();
+
+    //Hantera användarens val
+
+    if (val == "1")
+    {
+        Console.Clear();
+        Console.Write("SKRIV IN TEXT: ");
+        string text = Console.ReadLine();
+        File.WriteAllText("filnamn.txt", text);
+
+    }
+    else if (val == "2")
+    {
+        if (File.Exists("filnamn.txt"))
+        {
+        Console.Clear();
+        string text = File.ReadAllText("filnamn.txt");
+        Console.WriteLine($"{text}");
+        Console.ReadLine();
+        }
+        else
+        {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("FILEN FINNS INTE!");
+        Thread.Sleep(1500);
+        }
+
+    }
+    else if (val == "3")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("AVSLUTAR...");
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("VÄNLIGEN VÄLJ ETT GILTIGT ALTERNATIV");
+        Thread.Sleep(1500);
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.White;
