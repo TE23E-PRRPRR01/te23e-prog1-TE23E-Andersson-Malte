9c9e9a74dadf30b0b9c3b9ae10bc1b11a22d936d
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2024-11-11 14:53:29 +0100

    Tränat på int.tryParse() och metod

---
+++ Kapitel-5/SlumpadLista/Program.cs
@@ -0,0 +1,39 @@
+﻿// slumpa många tal och spara i lista
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("SLUMPA MÅNGA TAL OCH SPARA I LISTA");
+Console.WriteLine(" ");
+
+//Globala variabler
+//EN lista för heltal
+List<int> listaSlumptal = [];
+
+//Be användaren ange antal slumptal
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("Ange antal slumptal: ");
+int antal = int.Parse(Console.ReadLine());
+
+//Be användaren ange min & max slumptal
+Console.Write("Ange minvärde: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Ange maxvärde: ");
+int max = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+
+
+//Loopa 5 ggr
+Console.ForegroundColor = ConsoleColor.Yellow;
+for (int i = 0; i < antal; i++)
+{
+    //Slumpa ett tal 1-100
+    int slumptal = 0;
+    slumptal = Random.Shared.Next(min, max + 1);
+
+    //Lägg till slumptalet i listan
+    listaSlumptal.Add(slumptal);
+
+    Console.WriteLine($"Slumpat tal {i + 1}: {slumptal}");
+
+}
+
+Console.ForegroundColor = ConsoleColor.White;
---
+++ Kapitel-5/SlumpadLista2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Skapa lista med slumpade tal
+
+/* ************************************************************
+**                            Main                           **
+************************************************************ */
+
+// Validera användarens inmatning
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("GENERERA SLUMPTAL");
+
+//Deklarera variabler
+
+//Skapa en tom lista för att spara dess slumpade tal
+List<int> listaSlumptal = [];
+
+//Fråga igen tills användaren matar in ett giltigt svar
+int antal = LäsInHeltal();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Du vill ha {antal} slumptal!");
+Console.WriteLine(" ");
+
+//Användaren matar in min-värde
+int min = LäsInHeltal();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Ditt minvärde är {min}");
+Console.WriteLine(" ");
+
+//Användaren matar in max-värde
+int max = LäsInHeltal();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Ditt maxvärde är {max}");
+Console.WriteLine(" ");
+
+//Slumpa fram tal
+for (int i = 0; i < antal; i++)
+{
+    int slumptal = Random.Shared.Next(min, max + 1);
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine($"DITT SLUMPTAL ÄR {slumptal}");
+
+}
+
+/* ************************************************************
+**                     Mina egna metoder                     **
+ *********************************************************** */
+
+static int LäsInHeltal()
+{
+    //Fråga använderan om max-värde
+    int heltal = 0;
+    while (true)
+    {
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Ange maximumvärde: ");
+
+        bool lyckades = int.TryParse(Console.ReadLine(), out heltal);
+        if (lyckades)
+        {
+            break;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ange ett giltigt värde");
+        }
+    }
+    return heltal;
+}
