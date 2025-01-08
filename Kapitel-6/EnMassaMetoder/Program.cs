0478118f11681f1805f55b94054f6b24d3a146d0
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2025-01-08 15:03:19 +0100

    övat på metoder

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,36 @@
+﻿/* *****************************************************
+***********************M A I N**************************
+****************************************************** */
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+SägHejNamn("Greger");
+
+TalIKvadrat();
+
+/* *****************************************************
+********************M E T O D E R***********************
+****************************************************** */
+
+/// <summary>
+/// Skriv ut hälsning med namn
+/// </summary>
+/// <param name="namn">Namnet</param>
+
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Var hälsad {namn}!");
+}
+
+/// <summary>
+/// Metod för tal i kvadrat
+/// </summary>
+
+static void TalIKvadrat()
+{
+    Console.Write("Ange ett tal: ");
+    int tal = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Ditt tal i kvadrat: {tal*tal}");
+    
+}
---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,53 @@
+﻿/* *****************************************************
+**********************M A I N***************************
+****************************************************** */
+
+Console.Clear();
+Console.WriteLine("Hej Metoder!");
+
+//Anropa metoden
+SägHej();
+
+//Anropa metoden meny
+VisaMeny();
+
+//Anropa metoden registrear namn
+RegisterNamn();
+
+/* *****************************************************
+********************M E T O D E R***********************
+****************************************************** */
+
+/// <summary>
+/// Metod som skriver ut en hälsning
+/// </summary>
+
+static void SägHej()
+{
+    Console.WriteLine("Var hälsad!");
+}
+
+/// <summary>
+/// Metod för att visa meny
+/// </summary>
+
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut uppgifter
+    4. Avsluta
+    """);
+}
+
+/// <summary>
+/// metod för att ange namn
+/// </summary>
+
+static void RegisterNamn()
+{
+    Console.Write("Ange namn: ");
+    string namn = Console.ReadLine();
+    Console.WriteLine($"Hej, {namn}");
+}
