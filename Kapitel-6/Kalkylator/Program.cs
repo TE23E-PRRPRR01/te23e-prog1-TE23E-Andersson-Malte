72d84d1eeb28ee93f305ecac9a70b91c3841f6ee
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2025-01-22 15:05:43 +0100

    tränat på metoder och toppdown strategi

---
+++ Kapitel-6/Kalkylator/Program.cs
@@ -0,0 +1,105 @@
+﻿/**********
+Main
+**********/
+
+using System.Security.Claims;
+
+VisaPresentation();
+
+while (true)
+{
+    VisaMeny();
+    string val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        UtförAddition();
+    }
+    else if (val == "2")
+    {
+        UtförSubtraktion();
+    }
+    else if (val == "3")
+    {
+        UtförMultiplikation();
+    }
+    else if (val == "4")
+    {
+        UtförDivision();
+    }
+    else if (val == "5")
+    {
+        Console.WriteLine("Hejdå");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Försök igen");
+    }
+}
+
+/**********
+Metoder
+**********/
+
+/// <summary>
+/// visar Välkomsttext
+/// </summary>
+
+static void VisaPresentation()
+{
+    Console.Clear();
+    Console.WriteLine("""
+    ===========================
+    Välkommen till miniräknaren
+    ===========================
+    """);
+}
+
+static void VisaMeny()
+{
+    Console.Write("""
+    1. Addition
+    2. Subtraktion
+    3. Multiplikation
+    4. Division
+    5. Avsluta programmet
+    Ange ett alternativ: 
+    """);
+}
+
+static void UtförAddition()
+{
+    Console.Write("Ange första talet: ");
+    int förstaTalet = int.Parse(Console.ReadLine());
+    Console.Write("Ange andra talet: ");
+    int andraTalet = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{förstaTalet} + {andraTalet} = {förstaTalet + andraTalet}");
+}
+
+static void UtförSubtraktion()
+{
+    Console.Write("Ange första talet: ");
+    int förstaTalet = int.Parse(Console.ReadLine());
+    Console.Write("Ange andra talet: ");
+    int andraTalet = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{förstaTalet} - {andraTalet} = {förstaTalet - andraTalet}");
+}
+
+static void UtförMultiplikation()
+{
+    Console.Write("Ange första talet: ");
+    int förstaTalet = int.Parse(Console.ReadLine());
+    Console.Write("Ange andra talet: ");
+    int andraTalet = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{förstaTalet} * {andraTalet} = {förstaTalet * andraTalet}");
+}
+
+static void UtförDivision()
+{
+    Console.Write("Ange första talet: ");
+    int förstaTalet = int.Parse(Console.ReadLine());
+    Console.Write("Ange andra talet: ");
+    int andraTalet = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{förstaTalet} / {andraTalet} = {förstaTalet / andraTalet}");
+}
---
+++ Kapitel-6/MeddelandeApp/Program.cs
@@ -0,0 +1,97 @@
+﻿/***************
+******MAIN******
+***************/
+
+VisaPresentation();
+
+//Variabler
+string filnamn = "meddelande.txt";
+
+//Programloop
+while(true)
+{
+    VisaMeny();
+    string val = Console.ReadLine();
+    
+    if (val == "1")
+    {
+        SparaMeddelande(filnamn);
+    }
+    else if (val == "2")
+    {
+        LäsaMeddelande(filnamn);
+    }
+    else if (val == "3")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Avslutar...");
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Försök igen!");
+    }
+}
+
+/***************
+*****METODER****
+***************/
+
+/// <summary>
+/// Visar presentation av programmet
+/// </summary>
+
+static void VisaPresentation()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("""
+    --------------------------------------------
+     Program för att spara meddelande på disken
+    --------------------------------------------
+    """);
+}
+
+/// <summary>
+/// Visar menyn med alla funktioner för programmet
+/// </summary>
+
+static void VisaMeny()
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine();
+    Console.Write("""
+    1. Spara meddelande
+    2. Läs meddelande
+    3. Avsluta programmet
+    Ange ditt val: 
+    """);
+}
+
+/// <summary>
+/// Sparar meddelande från användaren
+/// </summary>
+
+static void SparaMeddelande(string filen)
+{
+    Console.Write("\nAnge ett meddelande: ");
+    string meddelande = Console.ReadLine();
+    File.WriteAllText(filen, meddelande);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Ditt meddelande har sparats!");
+
+}
+
+/// <summary>
+/// Läser upp senaste meddelandet
+/// </summary>
+
+static void LäsaMeddelande(string filen)
+{
+    string meddelande = File.ReadAllText(filen);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("\nDitt meddelande:");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(meddelande);
+}
